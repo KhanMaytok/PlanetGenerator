b4d01fed4901ac3c79fdfe6a345adddcbfda709c
Giancarlo Ventura <sakya_stelios@hotmail.com>
2016-12-06 18:26:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,12 @@ var rtable = new Array(65536);
 var gtable = new Array(65536);
 var btable = new Array(65536);
 
+var ftype = ['bmp','ppm','xpm'];
+
+var file_type = 'bmp';
+
+var letters = ['@','$','.',',',':',';','-','+','=','#','*','&','A','B','C','D',
+	'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T',
+	'U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j',
+	'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
